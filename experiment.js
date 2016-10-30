3e9fcc585695e3f397b517920d2fc7ae03eb11e0
marralyse <14riah@gmail.com>
2016-10-30 12:39:37 -0500

    if statment accounts for differentials

--- experiment.js
+++ experiment.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 var macros = ["pro", "carb", "fat"];
 var weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-var week = ["one", "two"];
+var week = ["one", "two", "three"];
 var differential = [];
 
 	//get the values for body fat percentage
@@ -50,134 +50,77 @@ var differential = [];
 
 	//Need to treat weight as an int
 	var weight = parseInt(weight);
-	//calculate Lean Body Mass
 	var LBM = weight + bodyFatTwo;
-	//calculate BMR-
 	var bmrNeg = Math.round(LBM * 10);
-	//calculate BRM+
 	var bmrPos = Math.round(LBM * 12);
-	//display LBM, BMR-, BMR+
 	$('#lbm').html(LBM);
 	$('#bmrneg').html(bmrNeg);
 	$('#bmrpos').html(bmrPos);
 
-	var protein = calcProtein(LBM);
-	console.log("protein: " + protein);
-
 	$("#water_intake").html("150.3 oz");
 	$("#fiber_intake").html("50.1 g");
 
-	$('#promon').html(protein + "g");
-	$('#protue').html(protein + "g");
-	$('#prowed').html(protein + "g");
-	$('#prothu').html(protein + "g");
-	$('#profri').html(protein + "g");
-	$('#prosat').html(protein + "g");
-	$('#prosun').html(protein + "g");
-
-	$('#promontwo').html(protein + "g");
-	carb = anyCalc(LBM, .4);
-	$('#carbmontwo').html(carb + "g");
-	fat = 	anyCalc(LBM, .3);
-	$('#fatmontwo').html(fat + "g");
-	$('#calmontwo').html(calcCalories(protein, carb, fat) + "g");
-
-	$('#protuetwo').html(protein + "g");
-	$('#prowedtwo').html(protein + "g");
-	$('#prothutwo').html(protein + "g");
-	$('#profritwo').html(protein + "g");
-	$('#prosattwo').html(protein + "g");
-	$('#prosuntwo').html(protein + "g");
-
-	$('#promonthree').html(protein + "g");
-	$('#protuethree').html(protein + "g");
-	$('#prowedthree').html(protein + "g");
-	$('#prothuthree').html(protein + "g");
-	$('#profrithree').html(protein + "g");
-	$('#prosatthree').html(protein + "g");
-	$('#prosunthree').html(protein + "g");
-
-	var carb = calcCarb(LBM);
-
-	$('#carbmon').html(carb + "g");
-	$('#carbtue').html(carb + "g");
-	$('#carbwed').html(carb + "g");
-	$('#carbthu').html(carb + "g");
-	$('#carbfri').html(carb + "g");
-	$('#carbsat').html(carb + "g");
-	$('#carbsun').html(carb + "g");
-
-	//$('#carbmontwo').html(anyCalc(LBM, .4) + "g");
-	$('#carbtuetwo').html(anyCalc(LBM, .4) + "g");
-	$('#carbwedtwo').html(anyCalc(LBM, 1.1) + "g");
-	$('#carbthutwo').html(anyCalc(LBM, .4) + "g");
-	$('#carbfritwo').html(anyCalc(LBM, .4) + "g");
-	$('#carbsattwo').html(anyCalc(LBM, 1.1) + "g");
-	$('#carbsuntwo').html(anyCalc(LBM, .4) + "g");
-
-	$('#carbmonthree').html(anyCalc(LBM, .4) + "g");
-	$('#carbtuethree').html(anyCalc(LBM, .4) + "g");
-	$('#carbwedthree').html(anyCalc(LBM, .7) + "g");
-	$('#carbthuthree').html(anyCalc(LBM, .4) + "g");
-	$('#carbfrithree').html(anyCalc(LBM, .4) + "g");
-	$('#carbsatthree').html(anyCalc(LBM, .7) + "g");
-	$('#carbsunthree').html(anyCalc(LBM, .4) + "g");
-
-	var fat = calcFat(LBM);
-	$('#fatmon').html(fat + "g");
-	$('#fattue').html(fat + "g");
-	$('#fatwed').html(fat + "g");
-	$('#fatthu').html(fat + "g");
-	$('#fatfri').html(fat + "g");
-	$('#fatsat').html(fat + "g");
-	$('#fatsun').html(fat + "g");
-
-	//$('#fatmontwo').html(anyCalc(LBM, .3) + "g");
-	$('#fattuetwo').html(anyCalc(LBM, .3) + "g");
-	$('#fatwedtwo').html(anyCalc(LBM, .3) + "g");
-	$('#fatthutwo').html(anyCalc(LBM, .3) + "g");
-	$('#fatfritwo').html(anyCalc(LBM, .3) + "g");
-	$('#fatsattwo').html(anyCalc(LBM, .3) + "g");
-	$('#fatsuntwo').html(anyCalc(LBM, .3) + "g");
-
-	$('#fatmonthree').html(anyCalc(LBM, .35) + "g");
-	$('#fattuethree').html(anyCalc(LBM, .35) + "g");
-	$('#fatwedthree').html(anyCalc(LBM, .35) + "g");
-	$('#fatthuthree').html(anyCalc(LBM, .35) + "g");
-	$('#fatfrithree').html(anyCalc(LBM, .35) + "g");
-	$('#fatsatthree').html(anyCalc(LBM, .35) + "g");
-	$('#fatsunthree').html(anyCalc(LBM, .35) + "g");
-
-	$('#calmon').html(calcCalories(protein, carb, fat) + "g");
-	$('#caltue').html(calcCalories(protein, carb, fat) + "g");
-	$('#calwed').html(calcCalories(protein, carb, fat) + "g");
-	$('#calthu').html(calcCalories(protein, carb, fat) + "g");
-	$('#calfri').html(calcCalories(protein, carb, fat) + "g");
-	$('#calsat').html(calcCalories(protein, carb, fat) + "g");
-	$('#calsun').html(calcCalories(protein, carb, fat) + "g");
-
-	//$('#calmontwo').html(calcCalories(protein, carb, fat) + "g");
-	$('#caltuetwo').html(calcCalories(protein, carb, fat) + "g");
-	$('#calwedtwo').html(calcCalories(protein, carb, fat) + "g");
-	$('#calthutwo').html(calcCalories(protein, carb, fat) + "g");
-	$('#calfritwo').html(calcCalories(protein, carb, fat) + "g");
-	$('#calsattwo').html(calcCalories(protein, carb, fat) + "g");
-	$('#calsuntwo').html(calcCalories(protein, carb, fat) + "g");
-
-	$('#calmonthree').html(calcCalories(protein, carb, fat) + "g");
-	$('#caltuethree').html(calcCalories(protein, carb, fat) + "g");
-	$('#calwedthree').html(calcCalories(protein, carb, fat) + "g");
-	$('#calthuthree').html(calcCalories(protein, carb, fat) + "g");
-	$('#calfrithree').html(calcCalories(protein, carb, fat) + "g");
-	$('#calsatthree').html(calcCalories(protein, carb, fat) + "g");
-	$('#calsunthree').html(calcCalories(protein, carb, fat) + "g");
-
-	//$('#calmontwo').html(protein(LBM, 1.1));
-
-
-
+var differential = 0;
+  for (var i = 0; i < week.length; i++){
+  for (var j = 0; j < weekdays.length; j++) {
+    for (var k = 0; k < macros.length; k++) {
+      name = "#" + macros[k] + weekdays[j] + week[i];
+      console.log(name);
+      //console.log("passing week: " + week[i] + " weekday: " + weekdays[j] + " macro: " + macros[k]);
+      differential = calcDifferential(week[i], weekdays[j], macros[k]);
+      console.log("calcdifferential returned " + differential);
+      //result = anyCalc(LBM, differential);
+      //$('name').html(result + 'g');
+    }
+  }
+}
 
 });
+
+function calcDifferential(week, weekdays, macros) {
+  //console.log("The week is " + week + "the weekday is " + weekdays + "the macro is " + macros);
+  //return true;
+  if (macros == "pro") {
+    console.log ("proteing equal 1.1")
+    return 1.1;
+  }
+  if (macros == "carb" && week == "one") {
+    return 1.5;
+  }
+  if (macros == "carb" && week == "two") {
+    if (weekdays == "wed" || weekdays == "sat") {
+      //console.log("the weeks should be two and is "  + week + "the weekday should be wed or sat. It is " + weekdays);
+      return 1.1;
+    }
+    else {
+    //console.log("the weeks should be two and is "  + week + "the weekday should not be wed or sat. It is " + weekdays);
+    return .4;
+    }
+  }
+  if (macros == "carb" && week == "three") {
+    if (weekdays == "wed" || weekdays == "sat") {
+        //console.log("the weeks should be three and is "  + week + "the weekday should be wed or sat. It is " + weekdays);
+      return .7;
+    }
+    else {
+        //console.log("the weeks should be three and is "  + week + "the weekday should not be wed or sat. It is " + weekdays);
+    return .4;
+    }
+  }
+  if (macros == "fat") {
+    if (week == "one") {
+      return .75;
+    }
+    if (week == "two") {
+      return .3;
+    }
+    else {
+      return .35;
+    }
+  }
+
+};
+
 //call the function
 function female(hip, waist, neck, height){
 	console.log("The function was called hip:(" + hip + ") waist: " + waist + " neck: " + neck + " height " + height);
@@ -197,28 +140,6 @@ function male(waist, neck, height) {
 function anyCalc(LBM, measurement){
 	console.log("fatTest: (LBM) " + LBM + " measurement" + measurement);
 	return Math.round(LBM * measurement);
-}
-
-
-function calcProtein(LBM)
-{
-	protein = LBM * 1.1;
-	protein = Math.round(protein);
-	return protein;
-}
-
-function calcCarb(LBM)
-{
-	carb = LBM * 1.5;
-	carb = Math.round(carb);
-	return carb;
-}
-
-function calcFat(LBM)
-{
-	fat = LBM * .75;
-	fat = Math.round(fat);
-	return fat;
 }
 
 function calcCalories(protein, carb, fat)
